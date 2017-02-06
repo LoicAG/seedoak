1465c19553ccb31ab98b0ae3dbce0dbbb5645113
Loic Antoine-Gombeaud <l.antoine-gombeaud@foodpanda.com>
2017-02-06 16:02:09 +0100

    Add Seed#show, fill Seed#create

--- app/controllers/seeds_controller.rb
+++ app/controllers/seeds_controller.rb
@@ -3,6 +3,18 @@ class SeedsController < ApplicationController
   end
 
   def create
-    render plain: params[:seed].inspect
+    @seed = Seed.new(seed_params)
+
+    @seed.save
+    redirect_to @seed
   end
+
+  def show
+    @seed = Seed.find(params[:id])
+  end
+
+  private
+    def seed_params
+      params.require(:seed).permit(:name, :description)
+    end
 end
