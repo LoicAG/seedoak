3c06fade10bf64f25c2bda907a3fb2e6071bc6b4
Loic Antoine-Gombeaud <l.antoine-gombeaud@foodpanda.com>
2017-02-07 12:09:05 +0100

    Add basic pick functionality

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -1,2 +1,16 @@
 class PicksController < ApplicationController
+  def new
+    @seed = Seed.find(params[:seed_id])
+  end
+
+  def create
+    @seed = Seed.find(params[:seed_id])
+    @pick = @seed.picks.create(pick_params)
+    redirect_to seed_path(@seed)
+  end
+
+  private
+    def pick_params
+      params.require(:pick).permit(:price)
+    end
 end
