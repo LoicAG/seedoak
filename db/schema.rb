d783532546e81b8366b64cbab6ca7dac2597da8b
Loic Antoine-Gombeaud <LoicAG@users.noreply.github.com>
2017-02-08 15:58:36 +0100

    User management (#1)
    * Add and install sorcery gem
    
    * Generate sorcery resources
    
    * Run sorcery migration
    
    * Add full scaffold for users (auto-generated)
    
    * Never show salt or encrypted password, ask for plain password and confirmation
    
    * Add validation for user fields
    
    * Add user_sessions controller (auto-generated)
    
    * Allow login/logout
    
    * Typo
    
    * Reset root, add link to login
    
    * Hide password field
    
    * Add links in app layout
    
    * Fix typo and routing
    
    * Remove duplicate notice div

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207093620) do
+ActiveRecord::Schema.define(version: 20170208115820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20170207093620) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",            null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
   add_foreign_key "picks", "seeds"
