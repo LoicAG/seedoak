2c44abd5f5899c5e9b5a65dde3e5731f86959dcb
Loic Antoine-Gombeaud <l.antoine-gombeaud@foodpanda.com>
2017-02-08 13:37:43 +0100

    Add user_sessions controller (auto-generated)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+
+  get 'user_sessions/create'
+
+  get 'user_sessions/destroy'
+
   resources :users
+
   resources :seeds do
     resources :picks
   end
