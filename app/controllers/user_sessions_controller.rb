26e35190969ab6666effed197ba9a8e1c91faf03
Loic Antoine-Gombeaud <l.antoine-gombeaud@foodpanda.com>
2017-02-10 16:55:57 +0100

    Fix redirect after logout

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -14,6 +14,6 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
-    redirect_to(:welcome, notice: 'Logged out!')
+    redirect_to(:root, notice: 'Logged out!')
   end
 end
